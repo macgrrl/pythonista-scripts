74f561bd77bd02533bbe98ac291d9830e880e09e
Kirschen Seah <kirschen@freerangecoder.com>
2020-01-11 11:18:24 -0600

    Tested and working update
    Calls Siri Shortcut "Instapaper URL" to add the passed URL to Instapaper

--- redirect_url_appex.py
+++ redirect_url_appex.py
@@ -1,28 +1,38 @@
 import appex
+import console
 import re
 import requests
 import webbrowser
 
-starturl = appex.get_text()
+# functions
+
+def show_alert(s):
+    console.alert('Alert', s, 'OK', hide_cancel_button=True)
+
+
+# Main program
+
+starturl = appex.get_url()
+
+#show_alert(starturl)
 
 response = requests.request('GET', starturl, allow_redirects=True)
 bin_content = response.content
 
 content = bin_content.decode('UTF-8')
-print(content)
+#show_alert(content)
 
 regex = re.compile(r'redirecturl = \'(https?://.*)\'')
 
 regex_match = regex.search(content)
 
 if regex_match != None:
-    print('Match made!')
     redirect_url = regex_match.group(1)
-    print(redirect_url)
+    #show_alert(redirect_url)
     # launch instapaper x-url
     #instapaper_xurl = 'x-callback-instapaper://x-callback-url/add?url='
     #response = requests.request('GET', instapaper_xurl + redirect_url)
-    webbrowser.open('workflow://run-workflow?name=Make%20PDF&input=text&text=soup
-redirect_url)
+    webbrowser.open('workflow://run-workflow?name=Instapaper%20URL&input=' + redirect_url)
 else:
-    print('No match...')
+    show_alert('No match...')
+
